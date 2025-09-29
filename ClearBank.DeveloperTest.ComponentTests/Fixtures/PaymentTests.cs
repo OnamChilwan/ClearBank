993dd915954f18cc9e97428e9c35ae9185158aae
Onam <onamch@gmail.com>
2025-09-29 22:15:39 +0100

    Added BDD-style component tests for `PaymentService` with scenarios for successful and unsuccessful payments, including account validation, payment scheme compatibility, and balance checks. These serve as a suite of tests to verify behaviour from an input / output perspective. Internals are covered by unit tests, such as verify account store is called to update the account whereas these tests are more for ensuring the behaviour hasn't changed. They allow for easier refactoring to take place which will be important for the next step.

--- ClearBank.DeveloperTest.ComponentTests/Fixtures/PaymentTests.cs
+++ ClearBank.DeveloperTest.ComponentTests/Fixtures/PaymentTests.cs
@@ -1,6 +1,184 @@
-﻿namespace ClearBank.DeveloperTest.ComponentTests.Fixtures;
+﻿using ClearBank.DeveloperTest.ComponentTests.Steps;
+using ClearBank.DeveloperTest.Types;
+using TestStack.BDDfy;
+
+namespace ClearBank.DeveloperTest.ComponentTests.Fixtures;
 
 public class PaymentTests
 {
+    private readonly PaymentSteps _steps = new();
     
+    [TestCase(PaymentScheme.Chaps, AllowedPaymentSchemes.Chaps)]
+    [TestCase(PaymentScheme.Bacs, AllowedPaymentSchemes.Bacs)]
+    [TestCase(PaymentScheme.FasterPayments, AllowedPaymentSchemes.FasterPayments)]
+    public void Successful_Payment(PaymentScheme scheme, AllowedPaymentSchemes allowedScheme)
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1000,
+            Status = AccountStatus.Live,
+            AllowedPaymentSchemes = allowedScheme
+        };
+
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = scheme,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsSuccessful())
+            .BDDfy();
+    }
+
+    [TestCase(PaymentScheme.Chaps)]
+    [TestCase(PaymentScheme.Bacs)]
+    [TestCase(PaymentScheme.FasterPayments)]
+    public void Unsuccessful_Payment_When_Account_Does_Not_Exist(PaymentScheme scheme)
+    {
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = "123",
+            PaymentScheme = scheme,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+        
+        this.Given(s => _steps.AccountDoesNotExist())
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful())
+            .BDDfy();
+    }
+
+    [Test]
+    public void Unsuccessful_BACs_Payment_When_Account_Does_Not_Support_Payment_Scheme()
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1000,
+            Status = AccountStatus.Live,
+            AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs
+        };
+        
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = PaymentScheme.Chaps,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful());
+    }
+
+    [Test]
+    public void Unsuccessful_Faster_Payment_When_Account_Does_Not_Support_Payment_Scheme()
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1000,
+            Status = AccountStatus.Live,
+            AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments
+        };
+        
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = PaymentScheme.Chaps,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful());
+    }
+
+    [Test]
+    public void Unsuccessful_Faster_Payment_Insufficient_Balance()
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1,
+            Status = AccountStatus.Live,
+            AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments
+        };
+        
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = PaymentScheme.FasterPayments,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful());
+    }
+    
+    [Test]
+    public void Unsuccessful_Chaps_Payment_When_Account_Does_Not_Support_Payment_Scheme()
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1000,
+            Status = AccountStatus.Live,
+            AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps
+        };
+        
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = PaymentScheme.Bacs,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful());
+    }
+
+    [TestCase(AccountStatus.Disabled)]
+    [TestCase(AccountStatus.InboundPaymentsOnly)]
+    public void Unsuccessful_Chaps_Payment_When_Account_Is_Not_Live(AccountStatus status)
+    {
+        var account = new Account
+        {
+            AccountNumber = "1234567890",
+            Balance = 1000,
+            Status = status,
+            AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps
+        };
+        
+        var request = new MakePaymentRequest
+        {
+            Amount = 100,
+            DebtorAccountNumber = account.AccountNumber,
+            PaymentScheme = PaymentScheme.Chaps,
+            CreditorAccountNumber = "999",
+            PaymentDate = DateTime.UtcNow
+        };
+
+        this.Given(s => _steps.AccountExists(account))
+            .When(s => _steps.MakingPayment(request))
+            .Then(s => _steps.PaymentIsUnsuccessful());
+    }
 }
--- ClearBank.DeveloperTest.ComponentTests/Steps/PaymentSteps.cs
+++ ClearBank.DeveloperTest.ComponentTests/Steps/PaymentSteps.cs
@@ -1,6 +1,56 @@
-﻿namespace ClearBank.DeveloperTest.ComponentTests.Steps;
+﻿using ClearBank.DeveloperTest.Configuration;
+using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Factories;
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using FluentAssertions;
+using NSubstitute;
+
+namespace ClearBank.DeveloperTest.ComponentTests.Steps;
 
 public class PaymentSteps
 {
+    private readonly PaymentService _paymentService;
+    private readonly IAccountDataStore _accountDataStore = Substitute.For<IAccountDataStore>();
+    private MakePaymentResult? _result;
+
+    public PaymentSteps()
+    {
+        _accountDataStore
+            .SupportedDataStoreType
+            .Returns(DataStoreType.Account);
+        
+        _paymentService = new PaymentService(
+            new PaymentConfiguration { DataStoreType = string.Empty },
+            new DataStoreFactory([ _accountDataStore ]));
+    }
     
+    public void AccountExists(Account account)
+    {
+        _accountDataStore
+            .GetAccount(account.AccountNumber)
+            .Returns(account);
+    }
+
+    public void AccountDoesNotExist()
+    {
+        _accountDataStore
+            .GetAccount(Arg.Any<string>())
+            .Returns((Account?)null); 
+    }
+
+    public void MakingPayment(MakePaymentRequest request)
+    {
+        _result = _paymentService.MakePayment(request);
+    }
+
+    public void PaymentIsSuccessful()
+    {
+        _result!.Success.Should().BeTrue();
+    }
+
+    public void PaymentIsUnsuccessful()
+    {
+        _result!.Success.Should().BeFalse();
+    }
 }
